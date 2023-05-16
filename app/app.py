8dce39e8102a084c6b79be5e281b4111ee69d15c
Siddarth Sam <sidsam248@gmail.com>
2023-05-17 00:21:56 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ import motor.motor_asyncio
 from controllers.courses_controller import CoursesController
 
 # Connect to the MongoDB instance
+# Should be in its own ENV variable. Handled statically for simplicity.
 client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://192.168.0.122:27017")
 
 # Select the MongoDB database
